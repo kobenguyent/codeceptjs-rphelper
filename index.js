e75316f56c21aecc04b1db7be27fee1b556e1262
Thanh Nguyen <peter.nguyentr@gmail.com>
2020-02-18 10:32:56 +0100

    rewrite reportportal as plugin

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ const RPClient = require('reportportal-client');
 const fs = require('fs');
 const path = require('path');
 const util = require('util');
-
-const Helper = codecept_helper;
+const event = require('codeceptjs').event;
+const Container = require('codeceptjs').container;
+const helpers = Container.helpers();
+let helper;
+let launchObj;
+let itemObj;
+let fileName;
+let stepInfo;
+let rpClient;
+let logFile;
+let suiteTempId;
+let beforeSuiteStatus = 'failed';
+let launchStatus = 'passed';
 
 const supportedHelpers = [
   'Mochawesome',
@@ -15,49 +26,86 @@ const supportedHelpers = [
   'TestCafe',
   'Playwright'
 ];
-let launchObj;
-let itemObj;
-let fileName;
-let stepInfo;
-let rpClient;
-let logFile;
-let suiteTempId;
-let beforeSuiteStatus = 'failed';
-let launchStatus = 'passed';
 
-class ReportPortalHelper extends Helper {
-  _updateStep(step, status) {
-    this._finishTestItem(launchObj, itemObj, step, status);
-  }
+for (const helperName of supportedHelpers) {
+	if (Object.keys(helpers).indexOf(helperName) > -1) {
+		helper = helpers[helperName];
+	}
+}
 
-  _passed() {
-    this._updateStep(stepInfo, 'passed');
-  }
+const defaultConfig = {
+  token: '',
+  endpoint: '',
+  project: '',
+  launchDescription: '',
+  attributes: [],
+  debug: false,
+  rerun: undefined,
+	enabled: false
+};
 
-  async _failed(test) {
+module.exports = (config) => {
+  config = Object.assign(defaultConfig, config);
+
+  event.dispatcher.on(event.all.before, () => {
+		launchObj = _startLaunch();
+  });
+
+  event.dispatcher.on(event.suite.before, (suite) => {
+    itemObj = _startTestItem(launchObj, suite.title, 'SUITE');
+    suiteTempId = itemObj.tempId;
+    beforeSuiteStatus = 'passed';
+  });
+
+  event.dispatcher.on(event.suite.after, () => {
+    if (stepInfo) {
+      rpClient.finishTestItem(suiteTempId, {
+        endTime: stepInfo.endTime,
+        status: beforeSuiteStatus,
+      });
+    } else {
+      rpClient.finishTestItem(suiteTempId, {
+        status: 'RESTED',
+      });
+    }
+  });
+
+  event.dispatcher.on(event.test.before, (test) => {
+    itemObj = _startTestItem(launchObj, test.title, 'STEP', suiteTempId);
+  });
+
+  event.dispatcher.on(event.test.passed, (test) => {
+    _updateStep(stepInfo, 'passed');
+  });
+  
+  event.dispatcher.on(event.test.failed, (test, err) => {
     launchStatus = 'failed';
-    this.errMsg = test.err.message;
-    this._updateStep(stepInfo, 'failed');
-  }
-
-  _startLaunch(suiteTitle) {
+    this.errMsg = err.message;
+    _updateStep(stepInfo, 'failed');
+  });
+  
+  event.dispatcher.on(event.test.finished, () => {
+    _finishLaunch(launchObj);
+  });
+  
+  function _startLaunch(suiteTitle) {
     rpClient = new RPClient({
       token: this.config.token,
       endpoint: this.config.endpoint,
       project: this.config.projectName,
       debug: this.config.debug,
     });
-
+  
     return rpClient.startLaunch({
       name: this.config.launchName || suiteTitle,
-      description: this.config.launchDescription || '',
-      attributes: this.config.launchAttributes || [],
-      rerun: this.config.rerun || false,
-      rerunOf: this.config.rerunOf || undefined,
+      description: this.config.launchDescription,
+      attributes: this.config.launchAttributes,
+      rerun: this.config.rerun,
+      rerunOf: this.config.rerunOf,
     });
   }
 
-  _startTestItem(launchObject, testTitle, method, suiteId = null) {
+  function _startTestItem(launchObject, testTitle, method, suiteId = null) {
     return rpClient.startTestItem({
       description: testTitle,
       name: testTitle,
@@ -65,15 +113,13 @@ class ReportPortalHelper extends Helper {
     }, launchObject.tempId, suiteId);
   }
 
-  _finishTestItem(launchObject, itemObject, step, status) {
+  function _finishTestItem(launchObject, itemObject, step, status) {
     if (step) {
       if (status === 'failed') {
-        supportedHelpers.forEach(async (helperName) => {
-          if (Object.keys(this.helpers).indexOf(helperName) > -1) {
-            this.helper = this.helpers[helperName];
+          if (helper) {
             fileName = `${rpClient.helpers.now()}_failed.png`;
             logFile = `${rpClient.helpers.now()}_browser.logs.txt`;
-            this.helper.saveScreenshot(fileName).then(() => {
+            helper.saveScreenshot(fileName).then(() => {
               rpClient.sendLog(itemObject.tempId, {
                 level: 'error',
                 message: `[FAILED STEP] ${stepInfo.actor} ${stepInfo.name} , ${stepInfo.args.join(',')} due to ${this.errMsg}`,
@@ -87,7 +133,7 @@ class ReportPortalHelper extends Helper {
               fs.unlinkSync(path.join(global.output_dir, fileName));
             });
 
-            this.helper.grabBrowserLogs().then((browserLogs) => {
+            helper.grabBrowserLogs().then((browserLogs) => {
               fs.writeFileSync(path.join(global.output_dir, logFile), util.inspect(browserLogs));
 
               rpClient.sendLog(itemObject.tempId, {
@@ -103,7 +149,7 @@ class ReportPortalHelper extends Helper {
               fs.unlinkSync(path.join(global.output_dir, logFile));
             });
           }
-        });
+
 
         rpClient.sendLog(itemObject.tempId, {
           level: 'error',
@@ -123,56 +169,17 @@ class ReportPortalHelper extends Helper {
     }
   }
 
-  _finishLaunch(launchObject) {
+  function _finishLaunch(launchObject) {
     rpClient.finishLaunch(launchObject.tempId, {
       status: launchStatus,
     });
   }
 
-  _beforeStep(step) {
-    if (step) {
-      stepInfo = step;
-    }
+  function  _updateStep(step, status) {
+    _finishTestItem(launchObj, itemObj, step, status);
   }
+  
+  return this;
+};
 
-  _afterStep(step) {
-    rpClient.sendLog(itemObj.tempId, {
-      level: 'info',
-      message: `[STEP] ${step.actor} ${step.name} , ${step.args.join(',')}`,
-      time: step.startTime,
-    });
-  }
-
-  _init() {
-    launchObj = this._startLaunch();
-  }
-
-  _beforeSuite(suite) {
-    itemObj = this._startTestItem(launchObj, suite.title, 'SUITE');
-    suiteTempId = itemObj.tempId;
-    beforeSuiteStatus = 'passed';
-  }
-
-  _afterSuite() {
-    if (stepInfo) {
-      rpClient.finishTestItem(suiteTempId, {
-        endTime: stepInfo.endTime,
-        status: beforeSuiteStatus,
-      });
-    } else {
-      rpClient.finishTestItem(suiteTempId, {
-        status: 'RESTED',
-      });
-    }
-  }
-
-  _finishTest() {
-    this._finishLaunch(launchObj);
-  }
-
-  _before(test) {
-    itemObj = this._startTestItem(launchObj, test.title, 'STEP', suiteTempId);
-  }
-}
-
-module.exports = ReportPortalHelper;
+module.exports = ReportPortal;
