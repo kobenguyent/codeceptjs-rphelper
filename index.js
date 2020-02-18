211b716ef30a0b35649d8e97cbc2896dff64ff83
Peter Nguyen Tr <peter.nguyentr@gmail.com>
2020-02-18 13:05:06 +0100

    testing phase

--- index.js
+++ index.js
@@ -6,15 +6,6 @@ const event = require('codeceptjs').event;
 const Container = require('codeceptjs').container;
 const helpers = Container.helpers();
 let helper;
-let launchObj;
-let itemObj;
-let fileName;
-let stepInfo;
-let rpClient;
-let logFile;
-let suiteTempId;
-let beforeSuiteStatus = 'failed';
-let launchStatus = 'passed';
 
 const supportedHelpers = [
   'Mochawesome',
@@ -28,9 +19,9 @@ const supportedHelpers = [
 ];
 
 for (const helperName of supportedHelpers) {
-	if (Object.keys(helpers).indexOf(helperName) > -1) {
-		helper = helpers[helperName];
-	}
+  if (Object.keys(helpers).indexOf(helperName) > -1) {
+    helper = helpers[helperName];
+  }
 }
 
 const defaultConfig = {
@@ -41,114 +32,142 @@ const defaultConfig = {
   attributes: [],
   debug: false,
   rerun: undefined,
-	enabled: false
+  enabled: false
 };
 
 module.exports = (config) => {
   config = Object.assign(defaultConfig, config);
 
+  let launchObj;
+  let suiteObj;
+  let testObj;
+  let fileName;
+  let stepInfo;
+  let stepObj;
+  let rpClient;
+  let logFile;
+  let beforeSuiteStatus = 'failed';
+  let launchStatus = 'PASSED';
+
   event.dispatcher.on(event.all.before, () => {
-		launchObj = _startLaunch();
+    launchObj = _startLaunch();
   });
 
   event.dispatcher.on(event.suite.before, (suite) => {
-    itemObj = _startTestItem(launchObj, suite.title, 'SUITE');
-    suiteTempId = itemObj.tempId;
-    beforeSuiteStatus = 'passed';
+    suiteObj = _startTestItem(launchObj, suite.title, 'SUITE');
+    beforeSuiteStatus = 'PASSED';
   });
 
-  event.dispatcher.on(event.suite.after, () => {
+  event.dispatcher.on(event.all.after, () => {
     if (stepInfo) {
-      rpClient.finishTestItem(suiteTempId, {
+      rpClient.finishTestItem(suiteObj.tempId, {
         endTime: stepInfo.endTime,
         status: beforeSuiteStatus,
       });
     } else {
-      rpClient.finishTestItem(suiteTempId, {
-        status: 'RESTED',
+      rpClient.finishTestItem(suiteObj.tempId, {
+        status: 'PASSED',
       });
     }
   });
 
   event.dispatcher.on(event.test.before, (test) => {
-    itemObj = _startTestItem(launchObj, test.title, 'STEP', suiteTempId);
+    testObj = _startTestItem(launchObj, test.title, 'TEST', suiteObj.tempId);
+  });
+
+  event.dispatcher.on(event.step.started, (step) => {
+    stepObj = _startTestItem(launchObj, step.toString(), 'STEP', testObj.tempId);
+  });
+
+  event.dispatcher.on(event.step.finished, (step) => {
+    console.log(step);
+    _finishTestItem(launchObj, stepObj, undefined, 'PASSED');
+  });
+
+  event.dispatcher.on(event.test.finished, (test) => {
+    _finishTestItem(launchObj, testObj, undefined, 'PASSED');
   });
 
   event.dispatcher.on(event.test.passed, (test) => {
-    _updateStep(stepInfo, 'passed');
+    _updateStep(stepInfo, 'PASSED');
   });
-  
+
   event.dispatcher.on(event.test.failed, (test, err) => {
     launchStatus = 'failed';
     this.errMsg = err.message;
     _updateStep(stepInfo, 'failed');
   });
-  
-  event.dispatcher.on(event.test.finished, () => {
+
+  event.dispatcher.on(event.all.after, () => {
     _finishLaunch(launchObj);
   });
-  
+
   function _startLaunch(suiteTitle) {
     rpClient = new RPClient({
-      token: this.config.token,
-      endpoint: this.config.endpoint,
-      project: this.config.projectName,
-      debug: this.config.debug,
+      token: config.token,
+      endpoint: config.endpoint,
+      project: config.projectName,
+      debug: config.debug,
     });
-  
+
     return rpClient.startLaunch({
-      name: this.config.launchName || suiteTitle,
-      description: this.config.launchDescription,
-      attributes: this.config.launchAttributes,
-      rerun: this.config.rerun,
-      rerunOf: this.config.rerunOf,
+      name: config.launchName || suiteTitle,
+      description: config.launchDescription,
+      attributes: config.launchAttributes,
+      rerun: config.rerun,
+      rerunOf: config.rerunOf,
     });
   }
 
   function _startTestItem(launchObject, testTitle, method, suiteId = null) {
-    return rpClient.startTestItem({
-      description: testTitle,
-      name: testTitle,
-      type: method,
-    }, launchObject.tempId, suiteId);
+    try {
+      return rpClient.startTestItem({
+        description: testTitle,
+        name: testTitle,
+        type: method,
+      }, launchObject.tempId, suiteId);
+    } catch (error) {
+      console.log(error);
+    }
+
   }
 
   function _finishTestItem(launchObject, itemObject, step, status) {
     if (step) {
       if (status === 'failed') {
-          if (helper) {
-            fileName = `${rpClient.helpers.now()}_failed.png`;
-            logFile = `${rpClient.helpers.now()}_browser.logs.txt`;
-            helper.saveScreenshot(fileName).then(() => {
-              rpClient.sendLog(itemObject.tempId, {
-                level: 'error',
-                message: `[FAILED STEP] ${stepInfo.actor} ${stepInfo.name} , ${stepInfo.args.join(',')} due to ${this.errMsg}`,
-                time: stepInfo.startTime,
-              }, {
-                name: fileName,
-                type: 'image/png',
-                content: fs.readFileSync(path.join(global.output_dir, fileName)),
-              });
-
-              fs.unlinkSync(path.join(global.output_dir, fileName));
+        if (helper) {
+          fileName = `${rpClient.helpers.now()}_failed.png`;
+          logFile = `${rpClient.helpers.now()}_browser.logs.txt`;
+          helper.saveScreenshot(fileName).then(() => {
+            rpClient.sendLog(itemObject.tempId, {
+              level: 'error',
+              message: `[FAILED STEP] ${stepInfo.actor} ${stepInfo.name} , ${stepInfo.args.join(',')} due to ${this.errMsg}`,
+              time: stepInfo.startTime,
+            }, {
+              name: fileName,
+              type: 'image/png',
+              content: fs.readFileSync(path.join(global.output_dir, fileName)),
             });
 
-            helper.grabBrowserLogs().then((browserLogs) => {
-              fs.writeFileSync(path.join(global.output_dir, logFile), util.inspect(browserLogs));
+            fs.unlinkSync(path.join(global.output_dir, fileName));
+          });
 
-              rpClient.sendLog(itemObject.tempId, {
-                level: 'trace',
-                message: `[BROWSER LOGS FOR FAILED STEP] ${stepInfo.actor} ${stepInfo.name} , ${stepInfo.args.join(',')} due to ${this.errMsg}`,
-                time: stepInfo.startTime,
-              }, {
-                name: logFile,
-                type: 'text/plain',
-                content: fs.readFileSync(path.join(global.output_dir, logFile)),
-              });
+          helper.grabBrowserLogs().then((browserLogs) => {
+            fs.writeFileSync(path.join(global.output_dir, logFile), util.inspect(browserLogs));
 
-              fs.unlinkSync(path.join(global.output_dir, logFile));
+            rpClient.sendLog(itemObject.tempId, {
+              level: 'trace',
+              message: `[BROWSER LOGS FOR FAILED STEP] ${stepInfo.actor} ${stepInfo.name} , ${stepInfo.args.join(',')} due to ${this.errMsg}`,
+              time: stepInfo.startTime,
+            }, {
+              name: logFile,
+              type: 'text/plain',
+              content: fs.readFileSync(path.join(global.output_dir, logFile)),
             });
-          }
+
+            fs.unlinkSync(path.join(global.output_dir, logFile));
+          });
+        }
 
 
         rpClient.sendLog(itemObject.tempId, {
@@ -163,23 +182,30 @@ module.exports = (config) => {
         status,
       });
     } else {
-      rpClient.finishTestItem(itemObject.tempId, {
-        status,
-      });
+      try {
+        rpClient.finishTestItem(itemObject.tempId, {
+          status,
+        });
+      } catch (error) {
+        console.log(error);
+      }
     }
   }
 
   function _finishLaunch(launchObject) {
-    rpClient.finishLaunch(launchObject.tempId, {
-      status: launchStatus,
-    });
+    try {
+      rpClient.finishLaunch(launchObject.tempId, {
+        status: launchStatus,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
   }
 
-  function  _updateStep(step, status) {
-    _finishTestItem(launchObj, itemObj, step, status);
+  function _updateStep(step, status) {
+    _finishTestItem(launchObj, stepObj, step, status);
   }
-  
+
   return this;
 };
-
-module.exports = ReportPortal;
