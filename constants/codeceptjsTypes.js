7f0cf357efac7affe15ba0391c48b9b03499c710
KobeNguyenT <7845001+kobenguyent@users.noreply.github.com>
2024-01-12 17:26:52 +0100

    feat: refactor logics (#26)

---
+++ constants/codeceptjsTypes.js
@@ -0,0 +1,24 @@
+const screenshotHelpers = [
+    'WebDriver',
+    'Appium',
+    'Puppeteer',
+    'TestCafe',
+    'Playwright',
+];
+
+const PREFIX_PASSED_TEST = '✅ [TEST]';
+const PREFIX_FAILED_TEST = '❌ [TEST]';
+const PREFIX_SKIPPED_TEST = '⏩ [SKIPPED TEST]'
+const PREFIX_PASSED_STEP = '✅ [STEP]';
+const PREFIX_FAILED_STEP = '❌ [STEP]';
+const PREFIX_BUG = '🐞🐞 LOGS --->'
+
+module.exports = {
+    screenshotHelpers,
+    PREFIX_PASSED_TEST,
+    PREFIX_FAILED_TEST,
+    PREFIX_SKIPPED_TEST,
+    PREFIX_PASSED_STEP,
+    PREFIX_FAILED_STEP,
+    PREFIX_BUG
+}
---
+++ helpers/rpHelpers.js
@@ -0,0 +1,181 @@
+const RPClient = require("@reportportal/client-javascript");
+const {LAUNCH_MODES} = require("../constants/launchModes");
+const {TEST_ITEM_TYPES} = require("../constants/testItemTypes");
+const {STATUSES} = require("../constants/statuses");
+const fs = require("fs");
+const path = require("path");
+const debug = require('debug')('codeceptjs:reportportal');
+const axios = require('axios').default;
+const restClient = axios.create();
+const { output, event } = codeceptjs;
+let rpClient;
+
+async function startLaunch(config, suiteTitle) {
+    rpClient = new RPClient({
+        apiKey: config.token,
+        endpoint: config.endpoint,
+        project: config.projectName,
+        debug: config.debug,
+    });
+
+    return rpClient.startLaunch({
+        name: config.launchName || suiteTitle,
+        description: config.launchDescription,
+        attributes: config.launchAttributes,
+        rerun: config.rerun,
+        rerunOf: config.rerunOf,
+        mode: LAUNCH_MODES.DEFAULT
+    });
+}
+
+async function finishLaunch(launchObj, launchStatus) {
+    try {
+        debug(`${launchObj.tempId} Finished launch: ${launchStatus}`)
+        const launch = rpClient.finishLaunch(launchObj.tempId, {
+            status: launchStatus,
+        });
+
+        const response = await launch.promise;
+        event.emit('reportportal.result', response);
+    } catch (error) {
+        console.log(error);
+        debug(error);
+    }
+}
+
+async function getRPLink(config, launchId) {
+    try {
+        const res = await restClient.get(`${config.endpoint}/${config.projectName}/launch?page.page=1&page.size=50&page.sort=startTime%2Cnumber%2CDESC`, { headers: { Authorization: `Bearer ${config.token}`}});
+        const launch = res.data.content.filter(item => item.uuid === launchId);
+        return `${config.endpoint.split('api')[0]}ui/#${config.projectName}/launches/all/${launch[0].id}`;
+    } catch (e) {
+        console.log(e);
+    }
+
+}
+
+function writePRInfo(launchLink, config) {
+    output.print(`📋 Writing results to ReportPortal: Project Name: ${config.projectName} > RP Endpoint: ${config.endpoint}`);
+    output.print(`📋 ReportPortal Launch Link: ${launchLink}`);
+}
+
+async function startTestItem(launchId, testTitle, method, parentId = null, parentTitle) {
+    try {
+        if (method === TEST_ITEM_TYPES.SUITE) {
+            const payload = suitePayload(testTitle)
+            return rpClient.startTestItem(payload, launchId, parentId);
+        }
+
+        if (method === TEST_ITEM_TYPES.TEST) {
+            const payload = testPayload(testTitle, parentTitle)
+            return rpClient.startTestItem(payload, launchId, parentId);
+        }
+
+        if ( method === TEST_ITEM_TYPES.STEP) {
+            const payload = stepPayload(testTitle, parentTitle)
+            return rpClient.startTestItem(payload, launchId, parentId);
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function finishStepItem(step) {
+    if (!step) return;
+
+    debug(`Finishing '${step.toString()}' step`);
+
+    return rpClient.finishTestItem(step.tempId, {
+        endTime: rpClient.helpers.now(),
+        status: rpStatus(step.status),
+    });
+}
+
+function logCurrent(data, file) {
+    const obj = stepObj || testObj;
+    if (obj) rpClient.sendLog(obj.tempId, data, file);
+}
+
+function rpStatus(status) {
+    if (status.toLowerCase() === 'success') return STATUSES.PASSED;
+    if (status.toLowerCase() === 'failed') return STATUSES.FAILED;
+    return status;
+}
+
+function suitePayload(title) {
+    return {
+        name: title,
+        type: TEST_ITEM_TYPES.SUITE,
+        codeRef: ''
+    }
+}
+
+function testPayload(testTitle, suiteTitle) {
+    return {
+        name: testTitle,
+        type: TEST_ITEM_TYPES.STEP,
+        codeRef: `${suiteTitle}/${testTitle}`,
+        testCaseId: `${suiteTitle}/${testTitle}`,
+        hasStats: true
+    }
+}
+
+function stepPayload(stepTitle, testTitle) {
+    return {
+        name: stepTitle,
+        type: TEST_ITEM_TYPES.STEP,
+        codeRef: `${testTitle}/${stepTitle}`,
+        testCaseId: `${testTitle}/${stepTitle}`,
+        hasStats: false
+    }
+}
+
+async function sendLogToRP({tempId, level, message, screenshotData}) {
+    debug(`📷 Attaching screenshot & error to failed step...`);
+    return rpClient.sendLog(tempId, {
+        level,
+        message,
+    }, screenshotData).promise;
+}
+
+async function attachScreenshot(helper, fileName) {
+    if (!helper) return undefined;
+    let content;
+
+    if (!fileName) {
+        fileName = `${rpClient.helpers.now()}_failed.png`;
+        try {
+            await helper.saveScreenshot(fileName);
+            content = fs.readFileSync(path.join(global.output_dir, fileName));
+            fs.unlinkSync(path.join(global.output_dir, fileName));
+        } catch (err) {
+            output.error('Couldn\'t save screenshot');
+            return undefined;
+        }
+    } else {
+        content = fs.readFileSync(path.join(global.output_dir, fileName));
+    }
+
+    return {
+        name: fileName,
+        type: 'image/png',
+        content,
+    };
+}
+
+module.exports = {
+    startLaunch,
+    getRPLink,
+    writePRInfo,
+    startTestItem,
+    finishStepItem,
+    logCurrent,
+    rpStatus,
+    suitePayload,
+    testPayload,
+    stepPayload,
+    sendLogToRP,
+    attachScreenshot,
+    finishLaunch
+}
